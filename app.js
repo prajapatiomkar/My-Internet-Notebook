b2a3b4c90678717c8649bb13c1254e3d93fafa4e
Prajapati omkar <prajapatiomkar01@gmail.com>
2022-09-01 12:54:53 +0530

    second commit Day 2

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.set("view engine", "ejs");
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
+let blogArray = [];
 
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render("home", { m_message: blogArray });
 });
 
 app.get("/about", (req, res) => {
@@ -20,6 +20,37 @@ app.get("/about", (req, res) => {
 app.get("/docs", (req, res) => {
     res.render("docs");
 });
+
+app.get("/create", (req, res) => {
+    res.render("create");
+});
+
+app.post("/create", (req, res) => {
+    let message = {
+        title: req.body.blog_title,
+        description: req.body.blog_description
+    }
+
+    blogArray.push(message);
+    res.redirect("/")
+});
+
+app.get("/delete", (req, res) => {
+    blogArray.pop();
+    res.redirect("/")
+})
+app.get("/clear", (req, res) => {
+   blogArray = []
+    res.redirect("/")
+})
+
+
+
+
+// app.deleteAlls("/deleteAll", (req, res) => {
+//     blogArray=[];
+//     res.redirect("/")
+// })
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server Started Listening at PORT 3000. http://localhost:3000");
